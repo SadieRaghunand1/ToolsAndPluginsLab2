61fc85a1ebddfd1a04106ef7b8fbd036c1750562
Makayla-ALD <ma654968@ucf.edu>
2025-09-06 00:44:10 -0400

    Button creation continued
    The buttons are functional and formatted, however the enable/disable button does not change color at the moment

--- Assets/ObjectScripts/CubeID.cs
+++ Assets/ObjectScripts/CubeID.cs
@@ -18,6 +18,7 @@ public class CubeEditor : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+        EditorGUILayout.BeginHorizontal();
 
         //Make button for selecting all cubes
         if (GUILayout.Button("Select all cubes"))
@@ -29,7 +30,27 @@ public class CubeEditor : Editor
             Selection.objects = allCubeObjects;
 
         }
-    }
+       
+        //Button for deselecting objects
+        if(GUILayout.Button("Clear selection")) 
+        {
+            Selection.objects = new Object[]  { (target as CubeID).gameObject };
+        }
+        EditorGUILayout.EndHorizontal();
+         
+        //Making the Disable and enable button, makes the cubes disappear and disables in hierarchy
+        if (GUILayout.Button("Disable/Enable all cubes", GUILayout.Height(40)))
+        //Trying to turn this button green(not working yet lol) 
+        {  
+             var cachedColor = GUI.backgroundColor;
+             GUI.backgroundColor = Color.green;
+             foreach (var cube in GameObject.FindObjectsOfType<CubeID>(true)) 
+             {
+             cube.gameObject.SetActive(!cube.gameObject.activeSelf);
+             }
+             GUI.backgroundColor = cachedColor;
+         }     
+     }
 }
 #endif
 
